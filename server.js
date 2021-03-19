ecb005fe6ba3eee6081de79462971ab6b17912c3
Nick <nteinert2005@gmail.com>
2021-03-19 16:36:53 -0500

    updated regis credendtials

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const redis = require('redis')
 const redisAdapter = require('socket.io-redis');
 const cors = require('cors')
 const path = require('path')
@@ -9,13 +10,20 @@ const PORT = process.env.PORT || 80
 const { addUser, getUser, deleteUser, getUsers } = require('./users')
 
 
+const REDIS_PORT = 11039;
+const REDIS_HOST = 'ec2-54-208-89-233.compute-1.amazonaws.com';
+
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// redis adapter
+const pubClient = redis.createClient(REDIS_PORT, REDIS_HOST, {
+    auth_pass: 'p551765675b247d78dddf9b57847dd89db0af13d4ab51b86fdf47f976ad7a365a'
+});
+const subClient = pubClient.duplicate();
 io.adapter(redisAdapter({
-    host: 'ec2-54-208-89-233.compute-1.amazonaws.com',
-    port: 11039
-}));
+    pubClient, subClient
+}))
 
 io.on('connection', (socket) => {
     socket.on('login', ({ name, room }, callback) => {
